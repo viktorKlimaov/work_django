72aea3a875433fcc51b39bdfc8b985bd44bc457a
viktor_klimanov <viktor.klimanov.2017@mail.ru>
2024-08-20 20:07:40 +0300

    создал новые шаблоны и контроллер

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/home.html', context)
 
 
 def contact(request):
     return render(request, 'catalog/contacts.html')
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk= pk)
+    context = {'product': product}
+    return render(request, 'catalog/product_detail.html', context)
