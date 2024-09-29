36cb3cfeea0aa5ac57cb77a121e168749b45f051
viktor_klimanov <viktor.klimanov.2017@mail.ru>
2024-09-29 16:01:28 +0300

    url настройки

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,20 +1,24 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import ProductListView, ProductDetailView, ContactView, ProductCreateView, ProductUpdateView, \
-    ProductDeleteView
+    ProductDeleteView, CategoryListView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/',ContactView.as_view(), name='contact'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
     path('product/create/', ProductCreateView.as_view(), name='product_form'),
     path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
+
+    path('categories/',CategoryListView.as_view(), name='category_list'),
 ]
 
 if settings.DEBUG:
